e7635bae7b0382c7e5d869d10ee05668330fce62
FelipeParrillo <62100348+FelipeParrillo@users.noreply.github.com>
2020-05-31 18:26:24 -0300

    Solo Documentacion y orden
    Puse las cosas mas ordenadas y bien documentadas, espaciadas

--- MAS PRUEBAS.py
+++ MAS PRUEBAS.py
@@ -71,45 +71,79 @@ ventana_juego = [[sg.Frame(layout=tabla.dibujar(), title="Tablero")],
                  ]
 
 
-###Funciones
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                               Funciones                                                              #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------- #
+#                           Funciones Atril                              #
+# ---------------------------------------------------------------------- #
+def bloquear_atril(letra):  #
+    """Bloquea la letra enviada del atril"""
+    window.FindElement(letra[0]).Update(button_color=("black", "grey"))
+    window.FindElement(letra[0]).Update(disabled=True)
+
+def desbloquear_atril(letra):
+    """Desbloquea la letra enviada del atril"""
+    window.FindElement(letra[0]).Update(button_color=("black", "white"))
+    window.FindElement(letra[0]).Update(disabled=False)
+
+def intercambio():
+    """Intercambia matrix, intercambia bloqueo y desbloqueo de atril, intercambia letras_usadas y intercambia tablero"""
+    global matrix, event, letras_usadas, letra_act, event_ant
+    if letra_act != " ":
+        letras_usadas.remove(matrix[event[0]][event[1]])
+        desbloquear_atril(matrix[event[0]][event[1]])
+        bloquear_atril(letra_act)
+        matrix[event[0]][event[1]] = letra_act
+        letras_usadas.append(letra_act)
+        window.FindElement(event).Update(letra_act)
+        letra_act = " "
+        event_ant = event
+# --------------------------------------------------------------------- #
+# --------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------- #
+#                           Funciones Tableros Simples                   #
+# ---------------------------------------------------------------------- #
+def bloquear_casilla(event):
+    """Bloquea la casilla mandada"""
+    if event != " ":
+        window.FindElement(event).Update(button_color=("black", "grey"))
+        window.FindElement(event).Update(disabled=True)
+
+def desbloquear_casilla(event):
+    """Desbloquea la casilla mandada"""
+    if event != " ":
+        window.FindElement(event).Update(button_color=("black", "green4"))
+        window.FindElement(event).Update(disabled=False)
+
 def desbloquear_tablero():
+    """Desbloquea_todo el tablero"""
     for x in range(15):
         for y in range(15):
             window.FindElement((x, y)).Update(disabled=False)
             window.FindElement((x, y)).Update(button_color=("black", "white"))
 
-
-def bloquear_tablero(event):  # bloquea todo menos el evento
+def bloquear_tablero(event):
+    """Bloquea_todo el tablero a excepcion de la posicion enviada, la cual modifica su color a verde"""
     for x in range(15):
         for y in range(15):
             if (x, y) != event:
                 window.FindElement((x, y)).Update(disabled=True)
                 window.FindElement((x, y)).Update(button_color=("black", "grey"))
     window.FindElement(event).Update(button_color=("black", "green4"))
+# --------------------------------------------------------------------- #
+# --------------------------------------------------------------------- #
 
 
-def bloquear_casilla(event):  # bloquea una casilla #no
-    if event != " ":
-        window.FindElement(event).Update(button_color=("black", "grey"))
-        window.FindElement(event).Update(disabled=True)
-
-def desbloquear_casilla(event):  # desbloquea una casilla #no
-    if event !=" ":
-        window.FindElement(event).Update(button_color=("black", "green4"))
-        window.FindElement(event).Update(disabled=False)
-
-
-def bloquear_atril(letra):  # bloquea una letra
-    window.FindElement(letra[0]).Update(button_color=("black", "grey"))
-    window.FindElement(letra[0]).Update(disabled=True)
-
-
-def desbloquear_atril(letra):  # desbloquea una letra
-    window.FindElement(letra[0]).Update(button_color=("black", "white"))
-    window.FindElement(letra[0]).Update(disabled=False)
-
-
-def desbloquear_adyascentes(event):  # desbloquea los adyascentes del evento
+# ---------------------------------------------------------------------- #
+#                           Funciones Tableros Complejas                 #
+# ---------------------------------------------------------------------- #
+def desbloquear_adyacentes(event):
+    """Desbloquea las casillas adyacentes a la posicion enviada, para que el usuario elija una orientacion"""
     if event[1] < 14:
         window.FindElement((event[0], event[1] + 1)).Update(disabled=False)
         window.FindElement((event[0], event[1] + 1)).Update(button_color=("black", "green4"))
@@ -120,56 +154,47 @@ def desbloquear_adyascentes(event):  # desbloquea los adyascentes del evento
         window.FindElement((event[0] - 1, event[1])).Update(disabled=False)
         window.FindElement((event[0] - 1, event[1])).Update(button_color=("black", "green4"))
 
+def bloqueo_desbloqueo_horientacion(event, event_ant):
+    """Bloquea y Desbloquea los casilleros, segun la orientacion que se elija"""
+    if event[0] == event_ant[0] - 1:  #
+        if event_ant[1] < 14:  #
+            bloquear_casilla((event_ant[0], event_ant[1] + 1))  #
+        if event_ant[0] < 14:  #
+            bloquear_casilla((event_ant[0] + 1, event_ant[1]))  #
+        if event[0] > 0:  #
+            desbloquear_casilla((event[0] - 1, event[1]))  #
+    if event[0] == event_ant[0] + 1:  #
+        if event_ant[0] > 0:  #
+            bloquear_casilla((event_ant[0] - 1, event_ant[1]))  #
+        if event_ant[1] < 14:  #
+            bloquear_casilla((event_ant[0], event_ant[1] + 1))  #
+        if event[0] < 14:  #
+            desbloquear_casilla((event[0] + 1, event[1]))  #
+    if event[1] == event_ant[1] + 1:  #
+        if event_ant[0] > 0:  #
+            bloquear_casilla((event_ant[0] - 1, event_ant[1]))  #
+        if event_ant[0] < 14:  #
+            bloquear_casilla((event_ant[0] + 1, event_ant[1]))  #
+        if event[1] < 14:  #
+            desbloquear_casilla((event[0], event[1] + 1))  #
+# ---------------------------------------------------------------------- #
+# ---------------------------------------------------------------------- #
 
-def intercambio():
-    global matrix
-    global event
-    global letras_usadas
-    global letra_act
-    global event_ant
-    if letra_act!=" ":
-        letras_usadas.remove(matrix[event[0]][event[1]])
-        desbloquear_atril(matrix[event[0]][event[1]])
-        bloquear_atril(letra_act)
-        matrix[event[0]][event[1]] = letra_act
-        letras_usadas.append(letra_act)
-        window.FindElement(event).Update(letra_act)
-        letra_act = " "
-        event_ant = event
 
-def is_empty(data_structure): #lo uso para informar si la lista de letras usadas esta vacia
+# ---------------------------------------------------------------------- #
+#                           Funciones Ayudantes                          #
+# ---------------------------------------------------------------------- #
+def is_empty(data_structure):
+    """Retorna True si la estructura esta vacia, o False en caso contrario"""
     if data_structure:
         return False
     else:
         return True
+# ---------------------------------------------------------------------- #
+# ---------------------------------------------------------------------- #
 
 
 
-
-def bloqueo_desbloqueo_horientacion(event, event_ant):
-    if event[0] == event_ant[0] - 1:  # bloquea y desbloquea los casilleros cuando elijo la orientacion hacia arriba
-        if event_ant[1] < 14:
-            bloquear_casilla((event_ant[0], event_ant[1] + 1))
-        if event_ant[0] < 14:
-            bloquear_casilla((event_ant[0] + 1, event_ant[1]))
-        if event[0] > 0:
-            desbloquear_casilla((event[0] - 1, event[1]))
-
-    if event[0] == event_ant[0] + 1:  # bloquea y desbloquea los casilleros cuando elijo la orientacion hacia abajo
-        if event_ant[0] > 0:
-            bloquear_casilla((event_ant[0] - 1, event_ant[1]))
-        if event_ant[1] < 14:
-            bloquear_casilla((event_ant[0], event_ant[1] + 1))
-        if event[0] < 14:
-            desbloquear_casilla((event[0] + 1, event[1]))
-
-    if event[1] == event_ant[1] + 1:  # bloquea y desbloquea los casilleros cuando elijo la orientacion horizontal
-        if event_ant[0] > 0:
-            bloquear_casilla((event_ant[0] - 1, event_ant[1]))
-        if event_ant[0] < 14:
-            bloquear_casilla((event_ant[0] + 1, event_ant[1]))
-        if event[1] < 14:
-            desbloquear_casilla((event[0], event[1] + 1))
 
 
 window = sg.Window("ScrabbleAR").Layout(ventana_juego)
@@ -188,34 +213,33 @@ while True:
 
     if (event in boton):
         if matrix[event[0]][event[1]] in letras_usadas:
-            if letra_act!=" ":
-                intercambio()  # cambia las letras en matrix y tablero,bloquea atril y desbloquea atril, tambien saca de letras_usadas la letra recuperada y guarda en letras_usadas la intercambiada, para colmo actualiza el evento anterior
+            if letra_act != " ":
+                intercambio()
             else:
                 letras_usadas.remove(matrix[event[0]][event[1]])
                 desbloquear_atril(matrix[event[0]][event[1]])
                 window.FindElement(event).Update(letra_act)
-                matrix[event[0]][event[1]] =None
+                matrix[event[0]][event[1]] = None
                 event_ant = event
                 if (is_empty(letras_usadas)):
                     desbloquear_tablero()
 
         else:
-            if elegir_direccion and event_ant != None and letra_act != " ":  # ingresa la segunda vez q pones una letra
+            if elegir_direccion and event_ant != None and letra_act != " ":
                 bloqueo_desbloqueo_horientacion(event, event_ant)
                 matrix[event[0]][event[1]] = letra_act
-                window.FindElement(event).Update(letra_act) # no
+                window.FindElement(event).Update(letra_act)
                 letras_usadas.append(letra_act)
                 bloquear_atril(letra_act)
                 event_ant = event
                 letra_act = " "
 
-            if event_ant is None and letra_act!= " ":  # aca ingresa la primera vez q pones una letra
-                window.FindElement(event).Update(letra_act)#no
+            if event_ant is None and letra_act != " ":
+                window.FindElement(event).Update(letra_act)
                 matrix[event[0]][event[1]] = letra_act
                 bloquear_tablero(event)
-                desbloquear_adyascentes(event)
+                desbloquear_adyacentes(event)
                 letras_usadas.append(letra_act)
                 bloquear_atril(letra_act)
                 event_ant = event
                 letra_act = " "
-
